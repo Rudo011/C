2b651908309512527264a581193dee5cbe50290f
Rudo011 <therudosuper@gmail.com>
2022-01-29 12:21:15 +0400

    //nor xndiri lucum

--- 2.cpp
+++ 2.cpp
@@ -1,28 +1,22 @@
-//xndir tpel shorti, longi, inti, floati ev doublei minimal ev maximal arjeqner@
-
 #include <iostream>
-#include <limits>
 using namespace std;
-
-
-template<typename T>
-void showMinMax() {
-   cout << "min: " << numeric_limits<T>::min() << endl;
-   cout << "max: " << numeric_limits<T>::max() << endl;
-   cout << endl;
-}
 int main()
-{
-	cout << "short:" << endl;
-   showMinMax<short>();
-   cout << "int:" << endl;
-   showMinMax<int>();
-   cout << "long:" << endl;
-   showMinMax<long>();
-   cout << "float:" << endl;
-   showMinMax<float>();
-   cout << "double:" << endl;
-   showMinMax<double>();
+{int i,x;
+	cout<<"Nermucel tiv  ";
+	cin>>x;
+	cout<<"2akan hamakargum =  ";
 
-	return 0;
+	for (i=31; i >= 0; i--)
+	{
+		if ((1 << i) & x)
+		{
+			cout<<"1";
+		}
+		else
+		{
+			cout<<"0";
+		}
+}
+cout<<endl;
+return 0;
 }

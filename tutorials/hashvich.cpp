5d01bba1ec0c7bd2174a03f311f325018171f841
Rudo011 <therudosuper@gmail.com>
2022-01-31 17:34:09 +0400

    ...

---
+++ tutorials/hashvich.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int a,b,c;
+
+			cout<<"Введите числа"<<endl;
+			cin>>a>>b;
+			cout<<"Введите ответ"<<endl;
+			cin>>c;
+			if (a*b==c){
+				cout<<"Ответ верный"<<endl;
+}
+
+			else {
+				cout<<"Ответ не верный"<<endl;
+				cout<<"Правильный ответ - "<<a*b<<endl;
+
+
+}
+
+
+
+
+
+
+return 0;
+}

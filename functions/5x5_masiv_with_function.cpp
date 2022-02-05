d753e6011d2ad230d32228146d1777dcd2aad375
Rudo011 <therudosuper@gmail.com>
2022-02-05 16:06:04 +0400

    5x5 masiv

--- functions/5x5_masiv_with_function.cpp
+++ functions/5x5_masiv_with_function.cpp
@@ -2,38 +2,64 @@
 #include <cstdlib>
 #include <ctime>
 
-int masiv [5][5] =  {};
+const int SIZE = 5;
 
-void fill()
-{
-	srand ( time ( NULL) );
-	
-	for ( int i = 0; i < 4; i++ )
-	{
-		for ( int p = 0; p < 4; p++ )
-		{
-			masiv[i][p] = 30 + rand() % 31;
-		}
-	}	
-
-
-}
-
-void max()
-{
-	for ( int i = 0; i < 4; i++ )
-	{
-		for ( int p = 0; p < 4; p++ )
-		{
-			std::max (masiv [i][p]);
-		}
-	}
-} 
-
+void fillandShow ( int [] [SIZE], const int size);
+int min ( int [] [SIZE], const int size);
+int max ( int [] [SIZE], const int size);
 
 int main()
 {
-	fill();
+	int masiv [SIZE][SIZE] = {};
+
+	fillandShow ( masiv, SIZE );
+	
+	std::cout << std::endl;
+	std::cout << "MAX = " << max(masiv, SIZE) << std::endl;
+	std::cout << "MIN = " << min(masiv, SIZE) << std::endl;
 
 	return 0;
+}
+
+void fillandShow ( int arr [] [SIZE], const int size)
+{
+	for ( int i = 0; i < size; i++ )
+	{
+		std::cout << "| ";
+		
+		for ( int j = 0; j < size; j++ )
+		{
+			arr [i] [j] = 30 + rand() % 31;
+			std::cout << arr[i][j] << " ";
+		}
+		std::cout << " |" << std:: endl;
+	}
+}
+
+int min ( int arr [][SIZE], const int size)
+{
+	int min = arr[0][0];
+	for ( int i = 0; i < size; i++ )
+	{
+		for ( int j = 0; j < size; j++ )
+		{
+			if ( arr[i][j] < min )
+			min = arr[i][j];
+		}
+	}
+	return min;
+}
+
+int max ( int arr [][SIZE], const int size)
+{
+	int max = arr [0][0];
+	for ( int i = 0; i < size; i++ )
+	{
+		for ( int j = 0; j < size; j++)
+		{
+			if ( arr[i][j] > max )
+			max = arr [i][j];
+		}
+	}
+	return max;
 }

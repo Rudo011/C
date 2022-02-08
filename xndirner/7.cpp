8b903d136e65368cdfb1960b3305bdd71900f94a
Rudo011 <therudosuper@gmail.com>
2022-02-08 14:43:04 +0400

    7.cpp

--- xndirner/7.cpp
+++ xndirner/7.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 int main()
 {
@@ -16,5 +17,36 @@ int main()
 	std::cout << "Введите номер группы студента: " << std::endl;
 	std::cin >> d;
 
-	std::cout << "*   Лабораторная работа №1   *" << std::endl;
-	std::cout << "*   Выполнил(а) ст.
+	int minErkarutyun = 31;
+	int erkrordToxiErkarutyun = 25 + strlen(d);
+	int erordToxiErkarutyun = strlen(a) + strlen(b) + strlen(c) + 6;
+
+	if ( erkrordToxiErkarutyun >= erordToxiErkarutyun && erkrordToxiErkarutyun > 31 )	
+		minErkarutyun = erkrordToxiErkarutyun;
+	else 
+		if ( erordToxiErkarutyun > 31 )
+		minErkarutyun = erordToxiErkarutyun;
+	std::cout << "\n\t\t\t";
+	for ( int x = 0; x < minErkarutyun; x++ )
+		std::cout << "*";
+
+	std::cout << "\n\t\t\t* Лабораторная работа №1";
+	for ( int x = 0; x <= ( minErkarutyun - erkrordToxiErkarutyun ); x++ )
+		std::cout << " ";
+
+	std::cout << "*\n\t\t\t* Выполнил(а): ст. гр. " << d;
+	for ( int x = 0; x <= ( minErkarutyun - erkrordToxiErkarutyun); x++ )
+		std::cout << " ";
+	
+	std::cout << "*\n\t\t\t* " << b << " " << a << " " << c;
+	for ( int x = 0; x <= ( minErkarutyun - erordToxiErkarutyun ); x++ )
+		std::cout << " ";
+	std::cout << "*";
+
+	std::cout << "\n\t\t\t";
+	for ( int x = 0; x < minErkarutyun; x++ )
+		std::cout << "*";
+	std::cout << std::endl;
+
+	return 0;
+}
